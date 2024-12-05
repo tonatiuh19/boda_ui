ea673f7af397ea61d92be5a0f2123e2c7def8d0c
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2024-12-04 18:49:17 -0600

    feat: new carousel

--- src/app/landing/shared/components/gallery-carousel/gallery-carousel.component.ts
+++ src/app/landing/shared/components/gallery-carousel/gallery-carousel.component.ts
@@ -1,17 +1,32 @@
-import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  OnDestroy,
+  AfterViewInit,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-gallery-carousel',
   templateUrl: './gallery-carousel.component.html',
   styleUrls: ['./gallery-carousel.component.css'],
 })
-export class GalleryCarouselComponent implements OnInit, OnDestroy {
+export class GalleryCarouselComponent
+  implements OnInit, OnDestroy, AfterViewInit
+{
   @Input() images: { name: string; path: string }[] = [];
+  @ViewChild('carouselTrack') carouselTrack!: ElementRef;
   currentIndex: number = 0;
   private intervalId: any;
 
   ngOnInit(): void {
     this.startCarousel();
+  }
+
+  ngAfterViewInit(): void {
+    this.updateCarousel();
   }
 
   ngOnDestroy(): void {
@@ -27,11 +42,26 @@ export class GalleryCarouselComponent implements OnInit, OnDestroy {
   }
 
   nextImage(): void {
-    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    this.currentIndex =
+      (this.currentIndex + 1) % Math.ceil(this.images.length / 3);
+    this.updateCarousel();
   }
 
   previousImage(): void {
     this.currentIndex =
-      (this.currentIndex - 1 + this.images.length) % this.images.length;
+      (this.currentIndex - 1 + Math.ceil(this.images.length / 3)) %
+      Math.ceil(this.images.length / 3);
+    this.updateCarousel();
+  }
+
+  updateCarousel(): void {
+    if (this.carouselTrack) {
+      const track = this.carouselTrack.nativeElement as HTMLElement;
+      const slideWidth =
+        track.querySelector('.carousel-slide')?.clientWidth || 0;
+      track.style.transform = `translateX(-${
+        this.currentIndex * slideWidth * 3
+      }px)`; // Move by 3 slides at a time
+    }
   }
 }
